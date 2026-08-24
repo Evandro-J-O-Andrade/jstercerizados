ded1e7b2b96efa2e8a490d4407cb09c2313e1a86
Evandro Andrade <45280444+Evandro-J-O-Andrade@users.noreply.github.com>
2026-08-24 10:30:15 -0300

    chore: align public layout with auth checkpoint

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -13,9 +13,7 @@ export function PublicLayout({ children }: PublicLayoutProps) {
     <div className="flex min-h-dvh flex-col overflow-x-hidden">
       <ScrollToTop />
       <Navbar />
-      <main className="flex-1 pt-16 pb-24 lg:pt-20 lg:pb-0">
-        {children || <Outlet />}
-      </main>
+      <main className="flex-1 pt-16 pb-24 lg:pt-20 lg:pb-0">{children}</main>
       <div className="pb-24 lg:pb-0">
         <Footer />
       </div>
